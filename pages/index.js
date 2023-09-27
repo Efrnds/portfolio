d1270a65bf1cf57f7094ec63503cf23ec020ca80
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2023-09-27 10:13:24 -0300

    oadsf

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const projects = [
     { title: "wiki-valley", href: "https://wiki-valley.github.io" },
     { title: "Cauadsm", href: "https://cauadsm.github.io" },
-    { title: "bikcraft", href: "/" },
+    { title: "bikcraft", href: "https://bikcraft.github.io/" },
     { title: "camera.tools", href: "/" },
     { title: "github", href: "/" },
     // Adicione mais projetos conforme necessário
@@ -17,15 +17,9 @@ export default function Home() {
       <p>Olá, sou o Eduardo, um apaixonado por front-end com foco em soluções criativas e resultados excepcionais. Minha jornada é marcada por anos de exploração em design responsivo, otimização de desempenho e integração perfeita de interfaces, capacitando-me a desenvolver sites responsivos e bem otimizados. Combinando expertise técnica e abordagem orientada para o cliente, estou comprometido em elevar cada projeto a um novo patamar. Explore meu portfolio e junte-se a mim em uma jornada de inovação e conquistas.</p>
       <h3>Meus Projetos:</h3>
       <div className={style.uga}>
-      {/* <Box title="bikcraft">
-      </Box> */}
       {projects.map((project, index) => (
         <Box key={index} title={project.title} href={project.href}/>
       ))}
-      {/* <Box title="Cauadsm">
-      </Box>
-      <Box title="camera.tools">
-      </Box> */}
       </div>
     </Layout>
   );
